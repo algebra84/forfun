450d99f4b2b740fea6bbe760d0937206304e3c00
algebra84 <crazylht@126.com>
2017-03-20 11:13:38 +0800

    version1

--- src/threedragon.cpp
+++ src/threedragon.cpp
@@ -1,33 +1,73 @@
 //3只精灵龙与复仇之怒的故事
 #include "threedragon.h"
-#if 0
+
 vector<int> data;
 void Bcktrack(int damage, double p);
+int alive = 0;
+double result = 0.0;
 
-double getResult(int damage, int dragon, int alive)
+void getResult(int damage, int dragon, int _alive)
 {
   data.push_back(30);
+  alive = _alive;
+  result = 0.0;
   for(int i = 0; i != dragon; i++)
-    dragon.push_back(2);
+    data.push_back(2);
   Bcktrack(damage, 1.00);
+    data.clear();
 }
 
-void Bcktrack(int damage, double p) //参数t表示当前递归深度
+int countAlive(bool dragon)
 {
-  if(damage == 0) ; //遍历到解，则将解输出或其他处理
+    int count = 0;
+    int i = dragon?1:0;
+    for(i; i != data.size(); i++)
+        if (data[i] != 0)
+            count++;
+    return count;
+}
+bool validate_result()
+{
+  return alive == countAlive(true);
+}
+void Bcktrack(int damage, double p)
+{
+  //遍历到解，则将解输出或其他处理
+  if(damage == 0) {
+    if (validate_result())
+      result += p;
+    return;
+  }
   else
     {
-      //f(n,t)和g(n,t)表示当前节点（扩展节点）处未搜索过的子树的起始编号和中指编号
-      for(int i=f(n,t);i<=g(n,t);i++)    
+      int count = countAlive(false);
+      if(count == 0)
+      {
+        cout<<"all dead"<<endl;
+        return;
+      }
+      double new_p = p/count;
+      //遍历所有子节点
+      for(int i=0;i != data.size();i++)
         {
-          x[t]=h(i);    //h(i)表示当前节点（扩展节点）处x[i]的第i个可选值
-          if(Constarint(t)&&Bound(t)) //剪枝函数：约束函数，限界函数
-            Bcktrack(t+1);
+          if(data[i] == 0)
+            continue;
+          data[i]--;
+          Bcktrack(damage - 1, new_p);
+          data[i]++;
         }
     }
+  return;
 }
-#endif
+
 int main()
 {
-  cout<<"hello"<<endl;
+    getResult(8,3,0);
+    cout<<"result of damage 8 dragon 3 alive 0: "<<result<<endl;
+    getResult(8,3,1);
+    cout<<"result of damage 8 dragon 3 alive 1: "<<result<<endl;
+    getResult(8,3,2);
+    cout<<"result of damage 8 dragon 3 alive 2: "<<result<<endl;
+    getResult(8,3,3);
+    cout<<"result of damage 8 dragon 3 alive 3: "<<result<<endl;
 }
